d9e827d807e6057cb3614c3ba8c870e5aa5d2c74
Sander Brustad <sander.simonsen.brustad@gmail.com>
2023-05-10 18:48:25 +0200

    Add temp header and footer

--- script/script.js
+++ script/script.js
@@ -1,2 +1,18 @@
 "use strict"
 
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+};
+
+function addBookToLibrary(book) {
+    myLibrary.push(book);
+};
+
+for(let i = 0; i < myLibrary.length; i++) {
+    
+}
